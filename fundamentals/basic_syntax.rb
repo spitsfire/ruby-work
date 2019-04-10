1823fe65a1776b17e1481458cd8d0881c8a0e57d
Claire Elliott <yue.katou@gmail.com>
2019-04-09 19:19:39 -0500

    initial commit

---
+++ fundamentals/basic_syntax.rb
@@ -0,0 +1,19 @@
+=begin
+    this is how to do long comments
+    blah balh
+    blah
+=end
+
+puts "hello" # new line carriage
+
+print "hello" # no new line carriage
+
+BEGIN {
+    puts "===== BEGINNING ====="
+}
+
+END {
+    puts "===== ENDING ====="
+}
+
+x = 5; y = 10; z = x+y # how to put assignment on ONE line
